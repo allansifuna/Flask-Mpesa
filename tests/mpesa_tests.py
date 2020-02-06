771143647cb7d551e3ea74545a4e864d6e1c6210
allansifuna <allansifuna324@gmail.com>
2020-02-06 17:25:42 +0300

    updating travis build

--- tests/mpesa_tests.py
+++ tests/mpesa_tests.py
@@ -1,13 +1,13 @@
 from flask import Flask
-from flask_mpesa import MpesaAPI
+# from flask_mpesa import MpesaAPI
 import click
 
 app=Flask(__name__)
 
-app.config["API_ENVIRONMENT"] = "sandbox"
-app.config["APP_KEY"] = "vbxsneeZ9IMFoyKKIgOIQQZFlawAADnP"
-app.config["APP_SECRET"] = "WAzDhQVhitIXwiTc"
-mp = MpesaAPI(app)
+# app.config["API_ENVIRONMENT"] = "sandbox"
+# app.config["APP_KEY"] = "vbxsneeZ9IMFoyKKIgOIQQZFlawAADnP"
+# app.config["APP_SECRET"] = "WAzDhQVhitIXwiTc"
+# mp = MpesaAPI(app)
 
 @click.command()
 def test():
