3a13c7593ea91bb7b7f33bec3bc486ece49cc43f
allansifuna <allansifuna324@gmail.com>
2020-02-06 17:31:17 +0300

    updating travis build

--- tests/mpesa_tests.py
+++ tests/mpesa_tests.py
@@ -1,14 +1,14 @@
 from flask import Flask
 # from flask_mpesa import MpesaAPI
 import click
-
+import os
 app=Flask(__name__)
 
 # app.config["API_ENVIRONMENT"] = "sandbox"
 # app.config["APP_KEY"] = "vbxsneeZ9IMFoyKKIgOIQQZFlawAADnP"
 # app.config["APP_SECRET"] = "WAzDhQVhitIXwiTc"
 # mp = MpesaAPI(app)
-
+TEST_PATH = HERE = os.path.abspath(os.path.dirname(__file__))
 @click.command()
 def test():
     """Run the tests."""
@@ -17,4 +17,4 @@ def test():
     exit(rv)
 
 if __name__ == '__main__':
-    app.run()
+    test()
